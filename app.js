69b895ce0894567ce970c7547307da11cf88c0b5
Ibtid <ibtid011@example.com>
2021-12-08 10:56:21 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('welcome azure');
+});
+
+app.listen(8080, () => {
+  console.log('Server listening on port 8080');
+});
